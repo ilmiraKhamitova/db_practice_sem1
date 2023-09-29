b388f783f9b548fab1e4a25b70bd7903daf70279
Ksyusha Skvortsova <116114350+giga-cherry@users.noreply.github.com>
2023-09-29 12:50:28 +0300

    Added ResumeTable and AdvertisersTable

---
+++ V1.12__createResumeTable.sql
@@ -0,0 +1,9 @@
+create table resume(
+	id bigint,
+	description varchar,
+	photo varchar,
+	employee_id bigint,
+	place_id bigint,
+	publication_date timestamp,
+	salary int
+);
---
+++ V1.13__createAdvertisersTable.sql
@@ -0,0 +1,6 @@
+create table advertisers(
+	contract_id bigint,
+	description varchar,
+	publication_date timestamp,
+	contract_price int
+);
