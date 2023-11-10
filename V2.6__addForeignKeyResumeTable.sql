84597610e4e3268ca956828e1d53c23dd85fd540
liiceberg <2aysylu2003@gmail.com>
2023-11-10 15:37:36 +0300

    extra file removed

--- V2.6__addForeignKeyResumeTable.sql
+++
@@ -1,3 +0,0 @@
-alter table resume 
-add foreign key (user_id) references usr (user_id) on delete cascade on update cascade,
-add foreign key (place_id) references walkingarea (id) on update cascade on delete set null
